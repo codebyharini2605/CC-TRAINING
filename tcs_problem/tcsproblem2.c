cd21cc3f22dedb60d229369cd4f4170ab5116c6e
thisizzharini <nithyaharini2625@gmail.com>
2026-07-21 15:46:08 +0530

    TcsProb2

---
+++ tcs_problem/tcsproblem2.c
@@ -0,0 +1,16 @@
+//Philaland Coin
+#include <stdio.h>
+
+int main()
+{
+    int n,count=0;
+    printf("Enter number:");
+    scanf("%d",&n);
+    while(n!=0){
+        n=n/2;
+        count=count+1;
+        //count++;
+    }
+    printf("%d",count);
+    return 0;
+}
